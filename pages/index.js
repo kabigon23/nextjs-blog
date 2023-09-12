f668b4a66109584e4e18433f056e1d0da2a3d5e3
kabigon23 <shaning7@gmail.com>
2023-09-12 23:37:43 +0900

    Insert Gukbab image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
-import Layout, {siteTitle} from '../components/layout';
+import Image from 'next/image';
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +14,16 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>안녕하세요 국밥의 블로그의 오신 것을 환영합니다.</p>
+        <div className={utilStyles.alignCenter}>
+          <Image
+            priority
+            src="/images/gukbab.jpeg"
+            height={360}
+            width={360}
+            alt="페이지용 프로필사진입니다."
+          />
+        </div>
+        
         <p>
           이 블로그는 Next.js로 만들어졌습니다.
         </p>
@@ -20,7 +31,7 @@ export default function Home({allPostsData}) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>블로그</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
