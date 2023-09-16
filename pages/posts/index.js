4e3b1a5fc449188cd3a89c351a5418d891d2984c
kabigon23 <shaning7@gmail.com>
2023-09-16 19:42:53 +0900

    Post

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -5,6 +5,7 @@ import utilStyles from "../../styles/utils.module.css";
 import styles from "../../components/layout.module.css";
 import TagPosts from "../../components/tagposts";
 import SearchPosts from "../../components/searchposts";
+import Head from "next/head";
 
 export default function Posts({ allPostsData }) {
     const [view, setView] = useState("All");
@@ -18,6 +19,9 @@ export default function Posts({ allPostsData }) {
 
     return (
         <Layout>
+            <Head>
+                <title>posts</title>
+            </Head>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <div className={styles.view}>
                     <button onClick={() => setView("All")}>All</button>
