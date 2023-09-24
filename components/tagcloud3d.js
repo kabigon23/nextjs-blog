bd4af20bd5812da5f1ed1ea4e3c0bf08b2dbd1e6
kabigon23 <shaning7@gmail.com>
2023-09-24 10:18:45 +0900

    test

--- components/tagcloud3d.js
+++ components/tagcloud3d.js
@@ -1,7 +1,10 @@
 import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';  // 1. Next.js router import
 
 const WordSphereComponent = ({ texts }) => {
     const canvasRef = useRef(null);
+    const router = useRouter();  // 2. useRouter hook
+
 
     const counts = [1, 2, 4, 5, 4, 2, 1];
     const options = {
@@ -17,13 +20,21 @@ const WordSphereComponent = ({ texts }) => {
 
         const canvas = canvasRef.current;
 
-        wordSphere(canvas, texts, counts, options);
+        wordSphere(canvas, texts, counts, options, router);
     }, [canvasRef, texts, counts, options]);
 
     return <canvas id="canvas" ref={canvasRef} />;
 };
 
-const wordSphere = (canvas, texts, counts, options) => {
+const wordSphere = (canvas, texts, counts, options, router) => {
+    let highlightedText = null;
+    const wordLinks = {
+        '자바스크립트': '#',
+        '일상': '#',
+        // ... (and so on)
+    };
+    let wordPositions = [];  // 6. 단어 위치 저장
+
     const π = Math.PI; // happy math!
     const {
         width = 320,
@@ -54,13 +65,30 @@ const wordSphere = (canvas, texts, counts, options) => {
 
     // scrolling
     let clicked = false, lastX, lastY;
-    let dx =0, dy=0;
+    let dx = 0, dy = 0;
     canvas.addEventListener('mousedown', event => {
         clicked = true;
         lastX = event.screenX;
         lastY = event.screenY;
     });
     canvas.addEventListener('mousemove', event => {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        highlightedText = null;
+
+        for (const { text, x: wordX, y: wordY, width, height } of wordPositions) {
+            if (
+                x >= wordX - width &&
+                x <= wordX + width &&
+                y >= wordY &&
+                y <= wordY + height
+            ) {
+                highlightedText = text;
+                break;
+            }
+        }
         if (!clicked) return;
         [dx, dy] = [event.screenX - lastX, event.screenY - lastY];
         [lastX, lastY] = [event.screenX, event.screenY];
@@ -77,22 +105,52 @@ const wordSphere = (canvas, texts, counts, options) => {
     });
     canvas.addEventListener('mouseup', e => clicked = false);
     canvas.addEventListener('mouseleave', e => clicked = false);
+    canvas.addEventListener('click', event => {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+    
+        for (const { text, x: wordX, y: wordY, width, height } of wordPositions) {
+            if (
+                x >= wordX - width && 
+                x <= wordX + width && 
+                y >= wordY && 
+                y <= wordY + height
+            ) {
+                const link = wordLinks[text];
+                if (link) {
+                    console.log(`Navigating to ${link}`);
+                    router.push(link);
+                }
+                break;
+            }
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+
 
     function rot(x, y, t) {
         return [x * Math.cos(t) - y * Math.sin(t), x * Math.sin(t) + y * Math.cos(t)];
     }
 
     function render() {
+        wordPositions = [];  // 초기화
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        let ix = 0, iz = 0, i = 1;
+        let ix = 0, iz = 0;
         for (const text of texts) {
             const degZ = (π / (counts.length - 1)) * iz;
             const degX = (2 * π / counts[iz]) * ix;
 
             let x = radius * Math.sin(degZ) * Math.cos(degX);
             let y = radius * Math.sin(degZ) * Math.sin(degX);
-            let z = radius * Math.cos(degZ) + 8 * (ix % 2) /* randomness */;
+            let z = radius * Math.cos(degZ) + 8 * (ix % 2); // randomness
 
             // camera transform
             [y, z] = rot(y, z, tilt);
@@ -104,16 +162,37 @@ const wordSphere = (canvas, texts, counts, options) => {
             const size = fontSize + 2 + 5 * (x / radius);
             ctx.fillStyle = `rgba(0,0,0,${alpha})`;
             ctx.font = `${size}px "Helvetica Neue", sans-serif`;
-            ctx.fillText(text, y + width / 2, -z + height / 2);
+
+            // 중심점의 좌표
+            const wordX = y + width / 2;
+            const wordY = -z + height / 2;
+
+            ctx.fillText(text, wordX, wordY);
+
+            // 텍스트의 위치를 저장합니다.
+            wordPositions.push({
+                text,
+                x: wordX,
+                y: wordY - size ,
+                width: ctx.measureText(text).width, // 텍스트의 너비를 저장
+                height: size // 텍스트의 높이를 저장
+            });
+
+            // 테두리를 그립니다.
+            if (highlightedText === text) {
+                ctx.strokeStyle = 'red';
+                const metrics = ctx.measureText(text);
+                ctx.strokeRect(wordX, wordY - size +2, metrics.width, size);
+            }
 
             ix--;
             if (ix < 0) {
                 iz++;
                 ix = counts[iz] - 1;
             }
-            i++;
         }
     }
+
 
     // renderer
     let looping = false;
@@ -126,7 +205,7 @@ const wordSphere = (canvas, texts, counts, options) => {
         if (vy > 0) vy = vy - 0.01;
         if (vx < 0) vx = vx + 0.01;
         if (vy > 0) vy = vy + 0.01;
-        if (vx == 0 && vy == 0) stopLoop();
+        // if (vx == 0 && vy == 0) stopLoop();
 
         rz += vy * 0.01;
         rx += vx * 0.01;
